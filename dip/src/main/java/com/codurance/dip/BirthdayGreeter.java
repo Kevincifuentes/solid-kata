c1bb6ee017d6ea190bb77d90d4633a0267f10c27
Kevin <kevin.cifuentes@opendeusto.es>
2021-12-07 13:07:20 +0100

    Update BirthdayGreeter.java

--- dip/src/main/java/com/codurance/dip/BirthdayGreeter.java
+++ dip/src/main/java/com/codurance/dip/BirthdayGreeter.java
@@ -4,12 +4,12 @@ import java.time.MonthDay;
 
 public class BirthdayGreeter {
     private final EmployeeRepository employeeRepository;
-    private final Publisher publisher;
+    private final EmailSender emailSender;
     private final TimeProvider timeProvider;
 
-    public BirthdayGreeter(EmployeeRepository employeeRepository, Publisher publisher, TimeProvider timeProvider) {
+    public BirthdayGreeter(EmployeeRepository employeeRepository, EmailSender emailSender, TimeProvider timeProvider) {
         this.employeeRepository = employeeRepository;
-        this.publisher = publisher;
+        this.emailSender = emailSender;
         this.timeProvider = timeProvider;
     }
 
