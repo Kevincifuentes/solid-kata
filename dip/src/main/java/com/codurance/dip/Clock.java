f45e3c0340a67c4d4b261fd286dd93e6e4a6c270
Kevin <kevin.cifuentes@opendeusto.es>
2021-12-07 13:15:03 +0100

    Update Clock.java

--- dip/src/main/java/com/codurance/dip/Clock.java
+++ dip/src/main/java/com/codurance/dip/Clock.java
@@ -2,7 +2,7 @@ package com.codurance.dip;
 
 import java.time.MonthDay;
 
-public class Clock {
+public class Clock implements TimeProvider {
     public MonthDay monthDay(){
         return MonthDay.now();
     }
