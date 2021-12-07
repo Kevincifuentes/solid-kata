e29f3d1719ba49e6a6536d4c9fd9a607c873b6fc
Kevin <kevin.cifuentes@opendeusto.es>
2021-12-07 13:07:48 +0100

    Update EmailSender.java

--- dip/src/main/java/com/codurance/dip/EmailSender.java
+++ dip/src/main/java/com/codurance/dip/EmailSender.java
@@ -1,6 +1,6 @@
 package com.codurance.dip;
 
-public class EmailSender implements Sender {
+public class EmailSender {
     public void sendAll(List<Email> emails) {
         emails.forEach(email -> System.out.print("To:"+email.getTo()+", Subject: "+email.getSubject()+", Message: "+email.getMessage()));
     }
